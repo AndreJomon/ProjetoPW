b58df139084e21bb0b7f08809aed83e1572b7b7e
Andre <jomonzito2010@gmail.com>
2020-06-05 16:36:35 -0300

    Deleted temp files

--- temp/metro.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-response = requests.get('http://www.metro.sp.gov.br/app/trajeto/xt/estacoesTipoXML.asp')  
-print(response.content)
--- temp/metro2.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-response = requests.get('http://apps.cptm.sp.gov.br:8080/AppMobileService/api/LinhasMetropolitanasAppv3?versao=4')  
-print(response.content)
